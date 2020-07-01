9330532878ac590b0d2f8d1ca7114391b2a109c4
pigstoe <67408203+pigstoe@users.noreply.github.com>
2020-07-01 11:27:16 +0900

    Add files via upload
    Improved structure of code.

--- Project Properties/pigstoe_Decrease project grid.lua
+++ Project Properties/pigstoe_Decrease project grid.lua
@@ -1,12 +1,19 @@
 -- Description: Decrease project grid
--- Version: 1.0
+-- Version: 1.0.1
 -- Author: pigstoe
 -- Website: http://blog.naver.com/pigstoe83
 
 
 
-local _, grid = reaper.GetSetProjectGrid(0, false)
-
-if grid > 0.0078125 then
-  reaper.GetSetProjectGrid(0, true, grid * 0.5)
+function Main()
+  local _, grid = reaper.GetSetProjectGrid(0, false)
+  
+  if grid > 0.0078125 then
+    reaper.GetSetProjectGrid(0, true, grid * 0.5)
+  end
 end
+
+reaper.PreventUIRefresh(1)
+Main()
+reaper.UpdateArrange()
+reaper.PreventUIRefresh(-1)
--- Project Properties/pigstoe_Increase project grid.lua
+++ Project Properties/pigstoe_Increase project grid.lua
@@ -1,12 +1,19 @@
 -- Description: Increase project grid
--- Version: 1.0
+-- Version: 1.0.1
 -- Author: pigstoe
 -- Website: http://blog.naver.com/pigstoe83
 
 
 
-local _, grid = reaper.GetSetProjectGrid(0, false)
-
-if grid < 4 then
-  reaper.GetSetProjectGrid(0, true, grid * 2)
+function Main()
+  local _, grid = reaper.GetSetProjectGrid(0, false)
+  
+  if grid < 4 then
+    reaper.GetSetProjectGrid(0, true, grid * 2)
+  end
 end
+
+reaper.PreventUIRefresh(1)
+Main()
+reaper.UpdateArrange()
+reaper.PreventUIRefresh(-1)
