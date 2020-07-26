1d07863425b635668d3848ee4102b1acc7b284f3
pigstoe <67408203+pigstoe@users.noreply.github.com>
2020-07-26 14:09:17 +0900

    Add files via upload
    Uploaded 2 new scripts.

---
+++ Items Editing/pigstoe_Increase length of selected MIDI items.lua
@@ -0,0 +1,50 @@
+-- Description: Increase length of selected MIDI items
+-- Version: 1.0.2
+-- Author: pigstoe
+-- Website: http://blog.naver.com/pigstoe83
+
+
+
+function Main()
+  local count_si = reaper.CountSelectedMediaItems(0)
+  if count_si < 1 then
+    reaper.defer(function() end)
+    return
+  end
+  
+  for i = 0, count_si - 1 do
+    local item = reaper.GetSelectedMediaItem(0, i)
+    local take = reaper.GetMediaItemTake(item, 0)
+    
+    local istmidi, inpmidi = reaper.BR_IsTakeMidi(take)
+    if not istmidi then
+      reaper.ShowMessageBox("Selected wav item.", "Wrong item", 0)
+      reaper.defer(function() end)
+      return
+    end
+  end
+
+  local _, grid = reaper.GetSetProjectGrid(0, false)
+  local len_unit = grid * 4
+  
+  reaper.Undo_BeginBlock()
+  for i = 0, count_si - 1 do
+    local item = reaper.GetSelectedMediaItem(0, i)
+    local pos0_s = reaper.TimeMap_timeToQN(reaper.GetMediaItemInfo_Value(item, "D_POSITION"))
+    local len0 = reaper.TimeMap_timeToQN(reaper.GetMediaItemInfo_Value(item, "D_LENGTH"))
+    local pos0_e = pos0_s + len0
+    
+    if math.fmod(pos0_e, len_unit) == 0 then
+      reaper.MIDI_SetItemExtents(item, pos0_s, pos0_e + len_unit)
+    else
+      local mul = math.floor(pos0_e / len_unit) + 1
+      reaper.MIDI_SetItemExtents(item, pos0_s, len_unit * mul)
+    end
+  end
+  reaper.Undo_EndBlock("Increase length of selected MIDI items", 4)
+end
+
+reaper.PreventUIRefresh(1)
+Main()
+reaper.UpdateArrange()
+reaper.PreventUIRefresh(-1)
