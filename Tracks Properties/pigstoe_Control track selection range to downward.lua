2c2075b00ceeb4ba153e9a5ffb5c9e1aeb187dd4
pigstoe <67408203+pigstoe@users.noreply.github.com>
2020-07-26 15:53:36 +0900

    Create pigstoe_Control track selection range to downward.lua
    Created a new script.

---
+++ Tracks Properties/pigstoe_Control track selection range to downward.lua
@@ -0,0 +1,33 @@
+-- Description: Control track selection range to downward
+-- Version: 1.0.1
+-- Author: pigstoe
+-- Website: http://blog.naver.com/pigstoe83
+
+
+
+function Main()
+  local count_st = reaper.CountSelectedTracks(0)
+  if count_st < 1 then
+    reaper.Main_OnCommand(40505, 0) -- Track: Select last touched track
+  elseif count_st == 1 then
+    reaper.Main_OnCommand(40287, 0) -- Track: Go to next track (leaving other tracks selected)
+  else
+    local trackc = reaper.GetLastTouchedTrack()
+    local idxc = reaper.CSurf_TrackToID(trackc, false)
+    local track0 = reaper.GetSelectedTrack(0, 0)
+    local idx0 = reaper.CSurf_TrackToID(track0, false)
+    if idxc > idx0 then
+      reaper.Main_OnCommand(40287, 0)
+    else
+      reaper.SetTrackSelected(trackc, false)
+      reaper.Main_OnCommand(40287, 0)
+    end
+  end
+  
+  reaper.SetCursorContext(0, nil)
+end
+
+reaper.PreventUIRefresh(1)
+Main()
+reaper.defer(function() end)
+reaper.PreventUIRefresh(-1)
