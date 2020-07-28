a8a06f7201c7cf6c2d679225ba589c60bacd4d88
pigstoe <67408203+pigstoe@users.noreply.github.com>
2020-07-28 10:38:14 +0900

    Add files via upload
    Added undo points and improved edit cursor behavior.

--- Items Editing/pigstoe_Move items and envelope points left by grid size.lua
+++ Items Editing/pigstoe_Move items and envelope points left by grid size.lua
@@ -1,5 +1,5 @@
 -- Description: Move items and envelope points left by grid size
--- Version: 1.0.3
+-- Version: 1.0.4
 -- Author: pigstoe
 -- Website: http://blog.naver.com/pigstoe83
 
@@ -7,28 +7,34 @@
 
 function Main()
   local count_si = reaper.CountSelectedMediaItems(0)
-  if count_si == 0 then return end
+  if count_si < 1 then
+    reaper.defer(function() end)
+    return
+  end
   
   local pos_s = math.huge
   
   for i = 0, count_si - 1 do
     local item = reaper.GetSelectedMediaItem(0, i)
-    if item ~= nil then
+    if not item then
       local pos_item = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
       pos_s = math.min(pos_s, pos_item)
     end
   end
   
   local len_nudge = reaper.BR_GetNextGridDivision(0)
-  if pos_s < len_nudge then return end
-  reaper.ApplyNudge(0, 0, 0, 1, -len_nudge , 0, 0)
-  
-  local pos_ps = pos_s - len_nudge
-  local pos_cur = reaper.BR_GetClosestGridDivision(pos_ps)
-  if pos_cur > pos_ps then
-    pos_cur = reaper.BR_GetPrevGridDivision(pos_cur)
+  if pos_s < len_nudge then
+    reaper.defer(function() end)
+    return
   end
-  reaper.SetEditCurPos(pos_cur, true, false)
+
+  local pos_cur = reaper.GetCursorPosition()
+  
+  reaper.Undo_BeginBlock()
+  reaper.ApplyNudge(0, 0, 0, 1, -len_nudge , 0, 0)
+  reaper.SetEditCurPos(pos_cur - len_nudge, true, false)
+  reaper.Undo_EndBlock("Move items and envelope points left by grid size", 4)
+  
   reaper.SetCursorContext(1, nil)
 end
 
--- Items Editing/pigstoe_Move items and envelope points right by grid size.lua
+++ Items Editing/pigstoe_Move items and envelope points right by grid size.lua
@@ -1,5 +1,5 @@
 -- Description: Move items and envelope points right by grid size
--- Version: 1.0.3
+-- Version: 1.0.4
 -- Author: pigstoe
 -- Website: http://blog.naver.com/pigstoe83
 
@@ -7,27 +7,29 @@
 
 function Main()
   local count_si = reaper.CountSelectedMediaItems(0)
-  if count_si == 0 then return end
+  if count_si < 1 then
+    reaper.defer(function() end)
+    return
+  end
   
   local pos_s = math.huge
   
   for i = 0, count_si - 1 do
     local item = reaper.GetSelectedMediaItem(0, i)
-    if item ~= nil then
+    if not item then
       local pos_item = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
       pos_s = math.min(pos_s, pos_item)
     end
   end
   
   local len_nudge = reaper.BR_GetNextGridDivision(0)
-  reaper.ApplyNudge(0, 0, 0, 1, len_nudge, 0, 0)
+  local pos_cur = reaper.GetCursorPosition()
   
-  local pos_ns = pos_s + len_nudge
-  local pos_cur = reaper.BR_GetClosestGridDivision(pos_ns)
-  if pos_cur > pos_ns then
-    pos_cur = reaper.BR_GetPrevGridDivision(pos_cur)
-  end
-  reaper.SetEditCurPos(pos_cur, true, false)
+  reaper.Undo_BeginBlock()
+  reaper.ApplyNudge(0, 0, 0, 1, len_nudge, 0, 0)
+  reaper.SetEditCurPos(pos_cur + len_nudge, true, false)
+  reaper.Undo_EndBlock("Move items and envelope points right by grid size", 4)
+  
   reaper.SetCursorContext(1, nil)
 end
 
