cd93399e5263a4fa3b673648961858d8a7f82dd9
pigstoe <67408203+pigstoe@users.noreply.github.com>
2020-07-30 18:25:56 +0900

    Add files via upload
    Uploaded two new scripts.

---
+++ MIDI Editor/pigstoe_Move edit cursor to next note on pitch cursor.lua
@@ -0,0 +1,36 @@
+-- Description: Move edit cursor to next note on pitch cursor
+-- Version: 1.0.1
+-- Author: pigstoe
+-- Website: http://blog.naver.com/pigstoe83
+
+
+
+function Main()
+  local hwnd = reaper.MIDIEditor_GetActive()
+  if not hwnd then return end
+  
+  local mode = reaper.MIDIEditor_GetMode(hwnd)
+  if mode ~= 0 then return end
+  
+  local take = reaper.MIDIEditor_GetTake(hwnd)
+  if not take then return end
+
+  local note_row = reaper.MIDIEditor_GetSetting_int(hwnd, "active_note_row")
+  local posc = reaper.MIDI_GetPPQPosFromProjTime(take, reaper.GetCursorPosition())
+  
+  local _, cnt_note = reaper.MIDI_CountEvts(take)
+  for i = 0, cnt_note - 1 do
+    local _, _, _, poss, _, _, pitch, _ = reaper.MIDI_GetNote(take, i)
+    if pitch == note_row then
+      if poss > posc then
+        reaper.SetEditCurPos(reaper.MIDI_GetProjTimeFromPPQPos(take, poss), true, false)
+        break
+      end
+    end
+  end
+end
+
+reaper.PreventUIRefresh(1)
+Main()
+reaper.defer(function() end)
+reaper.PreventUIRefresh(-1)
